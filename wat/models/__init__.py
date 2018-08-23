9890f76f657305ea5db6612f804be480af050202
Shreyas Kapur <shreyaskapur@yahoo.com>
2018-08-23 14:57:55 -0700

    Remove full text search.

--- wat/models/__init__.py
+++ wat/models/__init__.py
@@ -1,9 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy_searchable import make_searchable
 
 db = SQLAlchemy()
-
-make_searchable(db.metadata)
 
 import wat.models.user
 import wat.models.course
